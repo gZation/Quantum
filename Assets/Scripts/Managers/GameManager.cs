ce9a0ca7e8bf477fb125217df0db9df9923f5045
Joseph-Britt <48034902+Joseph-Britt@users.noreply.github.com>
2024-02-17 16:59:03 -0500

    Merge rn
    Group fixes for GameManager and PlayerManager from the work session

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -12,9 +12,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] private bool networkingOn = false;
     public bool startFromScene = true;
 
-    private int currentScene;
-    public GameObject shadowPrefab;
-
     [SerializeField] private GameObject shadow1;
     [SerializeField] private GameObject shadow2;
 
