bb06e80251763b97163e8afb82aa3d720311d997
AllisanLu <alcp2610@gmail.com>
2024-03-06 18:31:15 -0500

    Yay sprites shadow updates

--- Assets/Scripts/Puzzles/LevelAssetShadow.cs
+++ Assets/Scripts/Puzzles/LevelAssetShadow.cs
@@ -6,6 +6,12 @@ public class LevelAssetShadow : MonoBehaviour
 {
     public GameObject parent;
     public Vector3 offset = Vector3.zero;
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,6 +23,11 @@ public class LevelAssetShadow : MonoBehaviour
         else
         {
             this.transform.position = parent.transform.position + offset;
+            
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.sprite = parent.GetComponent<SpriteRenderer>().sprite;
+            }
         }
     }
 }
