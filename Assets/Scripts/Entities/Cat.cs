10b201f5c2c4d48ab23b073c4ff7a3dcc6069272
AllisanLu <alcp2610@gmail.com>
2024-03-06 17:50:37 -0500

    Made ending cat scene

---
+++ Assets/Scripts/Entities/Cat.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {
+            animator.SetBool("out", true);
+        }
+    }
+}
