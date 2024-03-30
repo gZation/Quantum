a513ae9bdc4b52d200662650ee27ae469742a1c8
Chase-rgb <wlee002@gmail.com>
2024-03-30 13:59:27 -0400

    Fix Networked Platforms

--- Assets/Scripts/Puzzles/MovingPlatform.cs
+++ Assets/Scripts/Puzzles/MovingPlatform.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
-public class MovingPlatform : MonoBehaviour
+public class MovingPlatform : NetworkBehaviour
 {
     public Vector3 direction;
     public float magnitude;
@@ -11,6 +12,8 @@ public class MovingPlatform : MonoBehaviour
     public bool oscillate = true;
     public float oscillateDuration = 2f;
     public Vector3 newD = new Vector3(0.0f, 0.0f, 0.0f);
+    private GameObject playerObject;
+    private Vector3 playerOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +22,25 @@ public class MovingPlatform : MonoBehaviour
         direction = direction.normalized * magnitude;
     }
 
+    public override void OnNetworkSpawn()
+    {
+        if (!IsHost) return;
+
+        //Connected client always has id of 1 (host is 0)
+        int clientId = 1;
+
+        //Check if curr moving platform is under Host's world grid level. If not, give ownership to client. 
+        string hostWorldLevelTag = "World" + PlayerManager.instance.currPlayer + "Level";
+        GameObject hostWorldGridLevel = GameObject.FindGameObjectWithTag(hostWorldLevelTag);
+        if (transform.parent != hostWorldGridLevel.transform)
+        {
+            this.GetComponent<NetworkObject>().ChangeOwnership((ulong) clientId);
+        }
+    }
+
     void FixedUpdate()
     {
+        if (GameManager.instance.IsNetworked() && !IsOwner) return;
         float bufferDuration = oscillateDuration * 0.25f;
         Vector3 acc = new Vector3(0.0f, 0.0f, 0.0f);
         if (oscillate)
@@ -51,13 +71,16 @@ public class MovingPlatform : MonoBehaviour
             }
         }
         transform.position += newD;
+
+        if (playerObject != null) playerObject.transform.position += newD;
     }
 
+    // Can't reparent NetworkObjects in splitscreen, so manually adjust player position
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            collision.transform.parent = transform;
+            playerObject = collision.gameObject;
         }
     }
 
@@ -65,7 +88,7 @@ public class MovingPlatform : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            collision.transform.parent = null;
+            playerObject = null;
         }
     }
 
