ab885cf371233a88be8ff41d115603e5c9124eec
AllisanLu <allison.lu@gatech.edu>
2024-04-03 15:28:48 -0400

    Restart level buttons

--- Assets/Scripts/Local Coop Scripts/MovementArrows.cs
+++ Assets/Scripts/Local Coop Scripts/MovementArrows.cs
@@ -6,26 +6,7 @@ public class MovementArrows : PlayerMovement
 {
     protected override Vector2 GetMovementDirection()
     {
-        Vector2 movementDirection = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            movementDirection.y += 1;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            movementDirection.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            movementDirection.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            movementDirection.x += 1;
-        }
-
-        return movementDirection.normalized;
+        return new Vector2(Input.GetAxis("HorizontalArrows"), Input.GetAxis("VerticalArrows"));
     }
 
     protected override Vector2 GetRawInput()
